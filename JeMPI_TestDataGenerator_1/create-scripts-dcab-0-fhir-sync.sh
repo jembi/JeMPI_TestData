a893e9759a61094f9897fd8274c181bee2ec1704
William Hayes <william.hayes@jembi.org>
2022-11-27 23:54:35 +0200

    Add scripgts to test sync FHIR

---
+++ JeMPI_TestDataGenerator_1/create-scripts-dcab-0-fhir-sync.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e 
+set -u
+
+mkdir -p scripts
+
+for CSV_FILE in test-data/*-dcab.csv.gz; do
+  BASE_NAME=$(basename $CSV_FILE .csv.gz)
+  CMND_NAME=`sed 's/data-test/test-01/g' <<<"$BASE_NAME-0-FHIR-SYNC"`
+  printf "#!/bin/bash
+
+set -e
+set -u
+
+FILE=%s
+
+DST_DIR=../../../JeMPI/JeMPI_Apps/JeMPI_FHIRsyncSender/csv
+
+pushd ../test-data
+  rm -f \$DST_DIR/*.csv
+  set -e
+  gzip -d -k \$FILE.csv.gz  
+  mv \$FILE.csv \$DST_DIR/\$FILE.temp
+  mv \$DST_DIR/\$FILE.temp \$DST_DIR/\$FILE.csv
+popd
+" $BASE_NAME > scripts/$CMND_NAME.sh
+  chmod u+x scripts/$CMND_NAME.sh  
+done  
+
